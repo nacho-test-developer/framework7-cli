38cd9eddc27b75088a19c37c6f59826bdcd9df14
yoelnacho <nacho.test.developer@gmail.com>
2020-03-24 17:01:13 -0300

    First commit

---
+++ src/pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <f7-page class="asd">
+    <f7-navbar title="Not found" back-link="Back"></f7-navbar>
+    <f7-block strong>
+      <p>Sorry</p>
+      <p>Requested content not found.</p>
+    </f7-block>
+  </f7-page>
+</template>
+<script>
+  export default {};
+</script>
+
+<style lang="scss">
+.asd {
+  color: red
+}
+</style>
